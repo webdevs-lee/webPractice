05b67575b5817b1e618bc53327e75d945f4f6e9a
webdevs-lee <jsweb1008@gmail.com>
2024-04-15 13:09:36 +0900

    FEAT: ToDoList 기능 구현 완료(Use LocalStorage)

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,31 +1,44 @@
 const todoInput = document.querySelector("#todo-input")
+const todoList = document.querySelector("#todo-list")
+const saveData = JSON.parse(localStorage.getItem("todoList"));
+
 todoInput.addEventListener("keypress", event => keyCodeCheck(event));
 
-const createTodo = () => {
+const createTodo = (data) => {
+    let text = data?.contents ?? todoInput.value;
+
     const newLi = document.createElement("li");
     const newSpan = document.createElement("span");
     const newBtn = document.createElement("button");
 
     newBtn.addEventListener("click", () => {
         newLi.classList.toggle("complete");
+        saveItems();
+
     })
 
     newLi.addEventListener("dblclick", () => {
         newLi.remove();
+        saveItems();
+
     })
 
-    newSpan.textContent = todoInput.value;
+    data?.complete ? newLi.classList.add("complete") : null;
+
+    newSpan.textContent = text;
+
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
-    console.log(newLi);
 
-    document.querySelector(".todo-container ul").appendChild(newLi);
+    todoList.appendChild(newLi);
 
-    todoInput.value = ""    
+    todoInput.value = ""
+    
+    saveItems();
 }
 
 const keyCodeCheck = (event) => {
-    if ((event.key == "Enter" || event.keyCode === 13) && todoInput.value !== "") {
+    if ((event.key === "Enter" || event.keyCode === 13) && todoInput.value !== "") {
         createTodo();
     }
     
@@ -37,4 +50,30 @@ const deleteAll = () => {
     for (const li of liList) {
         li.remove();
     }
+    localStorage.removeItem("todoList");
+
+}
+
+const saveItems = () => {
+    const saveItemsArray = [];
+    let todoObj = {};
+     // one
+     for (const todo of todoList.children) {
+        todoObj = {
+            contents: todo.querySelector("span").textContent,
+            complete: todo.classList.contains("complete")
+        };
+        
+        saveItemsArray.push(todoObj);
+     }
+
+     localStorage.setItem("todoList", JSON.stringify(saveItemsArray));
+}
+
+if (saveData) {
+    for (const data of saveData) {
+        createTodo(data);
+
+    }
+
 }
